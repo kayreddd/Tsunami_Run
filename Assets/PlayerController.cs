d0ce6c0132f32c65ac6eb856b5dbc080efe68157
kayreddd <148087656+kayreddd@users.noreply.github.com>
2025-11-04 14:01:52 +0100

    Story 1 done : creation of the player with horizontal and vertical movements

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,6 +1,29 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
-public class PlayerController
+
+public class PlayerController : MonoBehaviour
 {
-    
+    [SerializeField] private float moveSpeed = 2f; // Vitesse de déplacement du joueur
+    private Rigidbody2D rb;
+    private Vector2 movementDirection;     // Direction du mouvement selon les entrées clavier
+
+
+    void Start()
+    {
+        // On récupère le Rigidbody2D attaché à l'objet du joueur
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // On récupère les entrées de déplacement des flèches directionnelles
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movementDirection * moveSpeed; // multiplication de la direction par la vitesse choisie
+    }
 }
